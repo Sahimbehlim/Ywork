5e382b2cfdc68d6479c080dcfbacff2a45b3d242
Behlim Shaeem <behlimsahim@gmail.com>
2025-06-13 12:07:30 +0530

    update chatSection

--- src/app/components/Chats.jsx
+++ src/app/components/Chats.jsx
@@ -6,7 +6,7 @@ import ContactCard from "./common/ContactCard";
 export default function Chats() {
   const { contacts } = useChat();
   return (
-    <div className="bg-white w-1/4 border-r border-gray-200">
+    <div className="bg-white w-1/4 border-r border-gray-200 min-h-full h-max">
       <div className="px-4 py-5 font-semibold text-xl border-b border-gray-200">
         Chats
       </div>
